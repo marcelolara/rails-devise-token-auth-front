980660314165fa1d928e50ee5efb3e939203a5d2
Marcelo Lara <chelop@gmail.com>
2020-04-25 23:37:07 -0400

    Adding service error handler

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 import { User } from './user';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,32 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  /**
+   * Register a new user
+   * @param user, User object to register with email and password
+   * return Observable json
+   */
   register(user: User):Observable<any> {
     let api = `${this.endpoint}/auth`;
     return this.http.post(api, user)
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+
+  /**
+   * Manage Client side and server errors
+   * @param error
+   */
+  handleError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      msg = error.error.message;
+    } else {
+      // Server-side error
+      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(msg);
   }
 }
